256318e1e858c87de33962dc120825081965e7f0
Yashua <yashua@urbanlotus.com>
2014-05-20 10:21:33 -0700

    Mod slug file

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ var each = require('each');
  * Expose the integrations, using their own `name` from their `prototype`.
  */
 
-// Customize
-integrations = [
-  "adroll",
-  "adwords",
-  "bing-ads",
-  "errorception",
-  "facebook-ads",
-  "google-analytics",
-  "google-tag-manager",
-  "mixpanel",
-  "perfect-audience",
-  "twitter-ads"
-]
-
-
 each(integrations, function (slug) {
   var plugin = require('./lib/' + slug);
   var name = plugin.Integration.prototype.name;
